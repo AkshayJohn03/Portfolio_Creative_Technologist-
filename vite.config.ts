af436f1afdc3aaf727cb18203af7856f16ff45ec
AkshayJohn03 <akshay3rishi@gmail.com>
2026-03-09 00:11:47 +0530

    chore: Configure deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,22 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(process.cwd(), '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: '/Portfolio_Creative_Technologist-/',
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(process.cwd(), '.'),
       }
-    };
+    }
+  };
 });
